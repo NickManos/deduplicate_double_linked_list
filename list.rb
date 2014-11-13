d369676387746c5ea9787f8c2e960911347ff374
Nicholas Manos <manos.nick@gmail.com>
2014-11-13 13:46:32 -0800

    rubocop fix

--- list.rb
+++ list.rb
@@ -1,3 +1,4 @@
+# Node class. Represents a node.
 class Node
   def initialize(data)
     @data = data
@@ -6,13 +7,10 @@ class Node
   end
 
   attr_accessor :data, :next, :prev
-
 end
-
-
+# Double linked list class. Represents a Doubly linked list.
 class DLL
-
-  def initialize ()
+  def initialize
     @size = 0
     @head = nil
     @tail = nil
@@ -21,29 +19,16 @@ class DLL
   attr_accessor :size
 
   def remove(data)
-    return "The list is empty!" if @size == 0
+    return 'The list is empty!' if @size == 0
     index = @head
-    while index != nil
+    until index.nil
       if data == index.data
         # if the node to remove is at the head
-        if index.prev == nil
-          @head = index.next
-          index.next = nil
-          @size -= 1
-          return "Node with data #{index.data} successfully removed."
-        end
-        #if the node to remove is at the end
-        if index.next == nil
-          index.prev.next = nil
-          index.prev = nil
-          @size -= 1
-          return "Node with data #{index.data} successfully removed."
-        end
-        #if the node is in the middle somewhere
-        index.next.prev = index.prev
-        index.prev.next = index.next
-        @size -= 1
-        return "Node with data #{index.data} successfully removed."
+        remove_from_beginning(index)
+        # if the node to remove is at the end
+        remove_from_end(index)
+        # if the node is in the middle somewhere
+        remove_from_middle(index)
       end
       index = index.next
     end
@@ -52,67 +37,86 @@ class DLL
   def insert(data)
     node = Node.new(data)
     if @size == 0
-      @head = node
-      @tail = node
+      @head, @tail = node
     else
       @head.prev = node
       node.next = @head
       @head = node
     end
     @size += 1
-    return "Node with data #{node.data} successfully added."
+    "Node with data #{node.data} successfully added."
   end
 
   def rem_duplicates
-    return 'The list is empty' if @head == nil
-
+    return 'The list is empty' if @head.nil?
     node = @head
     hash = Hash.new(0)
-    while node != nil do
+    until node.nil?
       hash[node.data] = true if hash[node.data] != true
       node = node.next
     end
+    build_deduplicated_list(hash)
+  end
 
+  def to_s
+    node = @head
+    data = ''
+    return 'The list is empty' if @head.nil?
+    until node.nil?
+      data += "#{node.data}, "
+      node = node.next
+    end
+    data
+  end
+
+  def build_deduplicated_list(hash)
     key_array = hash.keys
     dll = DLL.new
 
     key_array.each do |data|
       dll.insert(data)
     end
-
-    return dll
-
+    dll
   end
 
-  def to_s
-    node = @head
-    data = ''
-    return 'The list is empty' if @head == nil
-    while node != nil do
-      data = data + "#{node.data}, "
-      node = node.next
+  def remove_from_beginning(index)
+    if index.prev.nil?
+      @head = index.next
+      index.next = nil
+      @size -= 1
+      "Node with data #{index.data} successfully removed."
     end
-    return data
   end
 
+  def remove_from_end(index)
+    return false unless index.next.nil?
+    index.prev.next = nil
+    index.prev = nil
+    @size -= 1
+    "Node with data #{index.data} successfully removed."
+  end
+
+  def remove_from_middle(index)
+    index.next.prev = index.prev
+    index.prev.next = index.next
+    @size -= 1
+    "Node with data #{index.data} successfully removed."
+  end
 end
 
-
-#Test code
+# Test code
 list = DLL.new
 numbers = (1..100).to_a
 
 200.times do
-list.insert numbers.sample
+  list.insert numbers.sample
 end
 
 puts ''
 puts 'Double Linked List with duplicates:'
 puts list.to_s
 puts ''
-puts'--------------------------------------------------------------------------'
+puts '------------------------------------------------------------------------'
 puts ''
 puts 'Double Linked List with duplicates removed:'
 puts list.rem_duplicates.to_s
-
-
